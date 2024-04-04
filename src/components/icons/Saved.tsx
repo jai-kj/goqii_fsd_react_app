91fd75d1191a06128918c04210c3ae48bf61face
jai-kj <jai.j@somaiya.edu>
2024-04-04 06:58:53 +0530

    feat(icons): added hero icons from tailwind

---
+++ src/components/icons/Saved.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Saved = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9"
+      />
+    </svg>
+  );
+};
+
+export default Saved;
